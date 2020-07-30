af82c2079552b8440bd650eee45148db820ed4af
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-07-30 09:46:32 -0300

    Initial Files, Before Procedural

---
+++ Player/Player.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+
+func _physics_process(delta):
+	var dir := Vector2.ZERO
+
+	if Input.is_action_pressed("ui_up"):
+		dir.y = -1
+
+	elif Input.is_action_pressed("ui_down"):
+		dir.y = 1
+
+	if Input.is_action_pressed("ui_left"):
+		dir.x = -1
+
+	elif Input.is_action_pressed("ui_right"):
+		dir.x = 1
+
+	self.move_and_slide(dir*20000*delta)
