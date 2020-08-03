18841a11f2e00e09e848f5dc45fc76f680a55c7c
Matheus Lima Cunha <matheus.limacunha@hotmail.com>
2020-08-03 19:33:00 -0300

    Initial Branching

--- roomcedural/map_generator/Map_Generator.gd
+++ roomcedural/map_generator/Map_Generator.gd
@@ -18,6 +18,7 @@ func _ready():
 		for _y in range(MAP_SIZE.y):
 			map_data[x].append(null)
 
+	var room_list : Array = []
 	var room_pos = START_POS
 	var from_direction : int
 	var exit_data : Array
@@ -26,17 +27,103 @@ func _ready():
 	$Room_Manager.prepare_room_list(room_types.START)
 
 	var start_room = $Room_Manager.get_room()
-	place_room(start_room, room_pos, $Tile_Map)
 	exit_data = choose_exit(start_room, room_pos)
+	start_room["map_position"] = room_pos
+	place_room(start_room, room_pos, $Tile_Map1)
 
-	from_direction = exit_data[0]
-	room_pos = exit_data[1]
+	room_list += make_path(exit_data[1], exit_data[0], 8, $Tile_Map1)
+
+	for _x in range(rand_range(1, 4)):
+		room_list.pop_front()
+
+	while true:
+		if room_list.empty():
+			print("Room List is Empty in Branch!")
+			exit_data = []
+			break
+
+		var branch_room = room_list.pop_front()
+		exit_data = choose_exit(branch_room, branch_room.map_position)
+
+		if not exit_data.empty():
+			break
+
+	if not exit_data.empty():
+		room_list += make_path(exit_data[1], exit_data[0], 6, $Tile_Map2)
+
+func dir_is_valid(room_pos : Vector2, direction : int) -> bool:
+	match(direction):
+
+		exit_dir.UP:
+			if (room_pos.y - 1) < 0 or map_data[room_pos.x][room_pos.y-1] != null:
+				return false
+
+		exit_dir.DOWN:
+			if (room_pos.y + 1) >= MAP_SIZE.y or map_data[room_pos.x][room_pos.y+1] != null:
+				return false
+
+		exit_dir.LEFT:
+			if (room_pos.x - 1) < 0 or map_data[room_pos.x-1][room_pos.y] != null:
+				return false
+
+		exit_dir.RIGHT:
+			if (room_pos.x + 1) >= MAP_SIZE.y or map_data[room_pos.x+1][room_pos.y] != null:
+				return false
+
+	return true
+
+func choose_exit(new_room : Dictionary, room_placement_pos := Vector2.ZERO) -> Array:
+	var exit_data : Array = []
+
+	for exit in new_room.exits:
+		#print("\tgetting exit from "+str(room_placement_pos+exit[0])+" in direction "+str(exit[1]))
+		if dir_is_valid(room_placement_pos+exit[0], exit[1]):
+			match(exit[1]):
+
+				exit_dir.UP:
+					exit_data.append(exit_dir.DOWN)
+					exit_data.append(room_placement_pos + exit[0] + Vector2.UP)
+
+				exit_dir.DOWN:
+					exit_data.append(exit_dir.UP)
+					exit_data.append(room_placement_pos + exit[0] + Vector2.DOWN)
+
+				exit_dir.LEFT:
+					exit_data.append(exit_dir.RIGHT)
+					exit_data.append(room_placement_pos + exit[0] + Vector2.LEFT)
+
+				exit_dir.RIGHT:
+					exit_data.append(exit_dir.LEFT)
+					exit_data.append(room_placement_pos + exit[0] + Vector2.RIGHT)
+
+			break
+
+	return exit_data
+
+func place_room(new_room : Dictionary, room_placement_pos : Vector2, tilemap : TileMap) -> void:
+	#print("\tmarking rooms in map_data")
+	for pos in new_room.positions:
+		var map_pos : Vector2 = room_placement_pos + pos
+		map_data[map_pos.x][map_pos.y] = new_room
+		#print("\t\t"+str(map_pos))
+
+	for tile in new_room.tiles:
+		var tile_pos : Vector2 = tile[0] + room_placement_pos*ROOM_SIZE
+		tilemap.set_cell(tile_pos.x, tile_pos.y, tile[1])
+
+func make_path(start_pos : Vector2, start_dir : int, path_limit : int = 4, Tile_Map = $Tile_Map1) -> Array:
+
+	var rooms_list : Array = []
+
+	var room_pos : Vector2 = start_pos
+	var from_direction : int = start_dir
 
 	#Create Normal Rooms
 	var created_rooms : int = 0
+	var exit_data : Array
 	$Room_Manager.prepare_room_list(room_types.NORMAL, from_direction)
 
-	while created_rooms < 50:
+	while created_rooms < path_limit:
 		#print("Attempting room in "+str(room_pos))
 		var new_room = $Room_Manager.get_room()
 
@@ -77,69 +164,13 @@ func _ready():
 			continue
 
 		created_rooms += 1
-		self.place_room(new_room, room_placement_pos, $Tile_Map)
+		new_room["map_position"] = room_placement_pos
+		rooms_list.append(new_room)
+		self.place_room(new_room, room_placement_pos, Tile_Map)
 
 		from_direction = exit_data[0]
 		room_pos = exit_data[1]
 
 		$Room_Manager.prepare_room_list(room_types.NORMAL, from_direction)
 
-func dir_is_valid(room_pos : Vector2, direction : int) -> bool:
-	match(direction):
-
-		exit_dir.UP:
-			if (room_pos.y - 1) < 0 or map_data[room_pos.x][room_pos.y-1] != null:
-				return false
-
-		exit_dir.DOWN:
-			if (room_pos.y + 1) >= MAP_SIZE.y or map_data[room_pos.x][room_pos.y+1] != null:
-				return false
-
-		exit_dir.LEFT:
-			if (room_pos.x - 1) < 0 or map_data[room_pos.x-1][room_pos.y] != null:
-				return false
-
-		exit_dir.RIGHT:
-			if (room_pos.x + 1) >= MAP_SIZE.y or map_data[room_pos.x+1][room_pos.y] != null:
-				return false
-
-	return true
-
-func place_room(new_room : Dictionary, room_placement_pos : Vector2, tilemap : TileMap) -> void:
-	#print("\tmarking rooms in map_data")
-	for pos in new_room.positions:
-		var map_pos : Vector2 = room_placement_pos + pos
-		map_data[map_pos.x][map_pos.y] = new_room
-		#print("\t\t"+str(map_pos))
-
-	for tile in new_room.tiles:
-		var tile_pos : Vector2 = tile[0] + room_placement_pos*ROOM_SIZE
-		tilemap.set_cell(tile_pos.x, tile_pos.y, tile[1])
-
-func choose_exit(new_room : Dictionary, room_placement_pos := Vector2.ZERO) -> Array:
-	var exit_data : Array = []
-
-	for exit in new_room.exits:
-		#print("\tgetting exit from "+str(room_placement_pos+exit[0])+" in direction "+str(exit[1]))
-		if dir_is_valid(room_placement_pos+exit[0], exit[1]):
-			match(exit[1]):
-
-				exit_dir.UP:
-					exit_data.append(exit_dir.DOWN)
-					exit_data.append(room_placement_pos + exit[0] + Vector2.UP)
-
-				exit_dir.DOWN:
-					exit_data.append(exit_dir.UP)
-					exit_data.append(room_placement_pos + exit[0] + Vector2.DOWN)
-
-				exit_dir.LEFT:
-					exit_data.append(exit_dir.RIGHT)
-					exit_data.append(room_placement_pos + exit[0] + Vector2.LEFT)
-
-				exit_dir.RIGHT:
-					exit_data.append(exit_dir.LEFT)
-					exit_data.append(room_placement_pos + exit[0] + Vector2.RIGHT)
-
-			break
-
-	return exit_data
+	return rooms_list
